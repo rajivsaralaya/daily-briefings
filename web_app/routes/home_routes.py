48a298d6350428b2fdd95c586e1ac0c85a377b1f
rajivsaralaya <104799236+rajivsaralaya@users.noreply.github.com>
2022-05-16 05:13:12 -0400

    gunicorn

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -8,14 +8,15 @@ home_routes = Blueprint("home_routes", __name__)
 @home_routes.route("/home")
 def index():
     print("HOME...")
-    return "Welcome Home"
-
+    #return "Welcome Home"
+    return render_template("home.html")
 
 @home_routes.route("/about")
 def about():
     print("ABOUT...")
-    return "About Me"
-
+   #return "About Me"
+    return render_template("about.html")
+    
 @home_routes.route("/another")
 def another():
     print("ANOTHER PAGE MAYBE...")
@@ -34,4 +35,5 @@ def hello_world():
    
    
     message = f"Hello, {name}!"
-    return message
+    #return message
+    return render_template("hello.html", message=message, other="YOLO")
